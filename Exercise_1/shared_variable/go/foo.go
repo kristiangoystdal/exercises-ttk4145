47e454181175bced8936fc53aacd71ecd5737864
Kristian Goystdal <krisgoy100@gmail.com>
2024-01-09 13:14:17 +0100

    rename of folders

---
+++ Exercise_1/shared_variable/go/foo.go
@@ -0,0 +1,35 @@
+// Use `go run foo.go` to run your program
+
+package main
+
+import (
+	"fmt"
+	. "fmt"
+	"runtime"
+	"time"
+)
+
+var i = 0
+
+func incrementing() {
+	for n := 0; n < 1000000; n++ {
+		i += 1
+	}
+	fmt.Println(i)
+}
+
+func decrementing() {
+	//TODO: decrement i 1000000 times
+}
+
+func main() {
+	// What does GOMAXPROCS do? What happens if you set it to 1?
+	runtime.GOMAXPROCS(2)
+
+	// TODO: Spawn both functions as goroutines
+
+	// We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
+	// We will do it properly with channels soon. For now: Sleep.
+	time.Sleep(500 * time.Millisecond)
+	Println("The magic number is:", i)
+}
